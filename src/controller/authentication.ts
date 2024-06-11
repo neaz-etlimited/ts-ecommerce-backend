c01c7c0c5689c3fe54d5a7b3d3583ebf275d71ca
neaz-etlimited <neaz@etlimited.net>
2024-06-11 17:15:57 +0600

    Done auth

---
+++ src/controller/authentication.ts
@@ -0,0 +1,54 @@
+import express from "express";
+import { authentication, random } from "../helpers";
+import { createUser, getUser, getUserByEmail } from "../db/users";
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password, userName } = req.body;
+
+    if (!email || !password || !userName) {
+      return res.sendStatus(400);
+    }
+
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+
+    const salt = random();
+
+    const user = await createUser({
+      email,
+      userName,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {}
+};
+
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+
+    if (user.authentication.password !== expectedHash) {
+      return res.sendStatus(400);
+    }
+  } catch (error) {}
+};
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import http from "http";
 import mongoose from "mongoose";
+import router from "./router";
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(express.json());
 
 const server = http.createServer(app);
 
-server.listen(8000, () => {
+server.listen(8001, () => {
   console.log("Server running");
 });
 
@@ -24,3 +25,5 @@ const MONGO_URL = "mongodb://localhost:27017/ecommerce";
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
